ff6c0c73f451e40d08c95a10c2b15f12f0d8adac
Mohammad ali mazdarani <mazdarani26.gmail.com>
2023-03-15 19:53:15 -0700

    Declare array and size of array

---
+++ Passin_argument_throught_pointer_sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system(
+) or input loop */
+//create namespace
+/*Branch name
+*
+*
+*/
+//prototype
+//--------------------------
+//micro
+//---------------------
+//global variable
+//-------------------
+int main(int argc, char** argv)
+{
+
+//Declare array size
+int size = 6;
+
+//Declare array
+int a[size] = {2,4,8,12,21,33};
+
+
+getch();
+}
+//function variable
+//--------------------------
+//functions
